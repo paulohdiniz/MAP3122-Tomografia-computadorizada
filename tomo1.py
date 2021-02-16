0f13c7fbffcd9e19dfdcd346357506fcb90ee73c
Paulo Henrique Diniz <ph.diniz@usp.br>
2021-02-15 22:45:18 -0300

    EP1 - Concluído

--- tomo1.py
+++ tomo1.py
@@ -1,56 +1,7 @@
-import math, matplotlib.pyplot as plt, numpy as np, pandas as pd, seaborn as sns, matplotlib.image as mpimg, pprint, sys, os
+import math, matplotlib.pyplot as plt, numpy as np, matplotlib.image as mpimg, pprint, sys, os
 from numpy import load
 
-#Fatoração e solução por LU
-def lenMatrix(matriz):
-	numero_linhas = len(matriz[:, 0]) 
-	numero_colunas = len(matriz[0, :])
-	return numero_linhas, numero_colunas
-
-def escalonamento(matriz):
-	array = matriz.copy()
-	multiplicadores = np.zeros([len(array[0,:]), len(array[:,0])])
-	for i in range(len(array[:, 0])-1):
-		for k in range(i+1, len(array[:,0])):
-			multiplicadores[k, i]  = array[k, i]/array[i,i]
-			linhaTemp = array[i,] * multiplicadores[k, i]
-			linhaFinal = array[k,] - linhaTemp
-			array[k] = linhaFinal
-	for i in range(len(multiplicadores)):
-		multiplicadores[i, i] = 1
-	return array, multiplicadores
-
-def resolveTriangularInf(matrizTriangular, respostas):
-	linhas,colunas = lenMatrix(matrizTriangular)
-	R = respostas
-	M = matrizTriangular
-	X  = np.zeros([linhas, 1])
-	for i in range(linhas):
-		X[i] = R[i]
-		for j in range(i):
-			X[i] -= M[i][j]*X[j]
-		X[i] /= M[i][i]
-	return X
-
-def resolveTriangularSup(matrizTriangular, respostas):
-	linhas,colunas = lenMatrix(matrizTriangular)
-	R = respostas
-	M = matrizTriangular
-	X  = np.zeros([linhas, 1])
-	for i in range(linhas-1,-1,-1):
-		X[i, 0] = R[i, 0]
-		for k in range(i+1, linhas):
-			X[i, 0] -= M[i][k]*X[k][0]
-		X[i, 0] /= M[i][i]
-	return X
-
-def achaSolucao(M, R):
-	upper, lower  = escalonamento(M)
-	Y = resolveTriangularInf(lower, R)
-	X = resolveTriangularSup(upper, Y)
-	return X
-
-# funções básicas
+# funções básicas/auxiliares
 
 # Identidade ordem n
 def Identidade(n):
@@ -67,37 +18,14 @@ def Determinante(A):
     B = np.linalg.det(A)
     return B
 
-# Inversa
-def Inversa (A):
-    B = np.linalg.inv(A)
-    return B
-
 #Devolve matriz mxn com zeros
 def MatrizZero (m, n):
     A = np.zeros((m,n))
     return A
 
-#Diagonal Secundaria de UNS
-def DiagSec (ordem):
-    A = np.fliplr(np.diag(np.ones(ordem)))
-    return A
-
-#funcoes particulares
-
-#Matriz n-ésima linha de UNS
-def Xesima (x, lin, col): 
-    XesimaM = [[0 for x in range(lin)] for y in range(col)] 
-    for i in range(lin):
-        for j in range(col):
-            if i == x-1:
-                XesimaM[i][j] = 1
-            else:
-                XesimaM[i][j] = 0
-    return XesimaM
-
 #Devolve matriz mxn com zeros e linha x com UNS
 def MatrizNUns (m, n, x):
-    B = np.zeros((m,n))
+    B = MatrizZero(m,n)
     for i in range(m):
         for j in range(n):
             if i == x-1:
@@ -106,18 +34,7 @@ def MatrizNUns (m, n, x):
                 B[i][j] = 0
     return B
 
-def deslocaMatriz(A):
-    m = A.shape[0]
-    n = A.shape[1]
-    B = MatrizZero(m,n)
-    for i in range(m):
-        for j in range(n):
-            if i == 0 or j ==0:
-                B[i][j] = 0
-            else:
-                B[i][j] = A[i-1][j-1]
-    return B
-
+#plota o resultado das imagens do programa
 def plotaImages(image, sol1, sol2, sol3):
     plt.figure(figsize=(10,6))
     plt.subplot(2,2,1)
@@ -143,6 +60,7 @@ def plotaImages(image, sol1, sol2, sol3):
 
 
     plt.show()
+
 #Matriz A
 def MatrizA (entrada):
     ParteSuperior = np.kron(MatrizNUns(1, entrada, 1), Identidade(entrada))
@@ -154,6 +72,77 @@ def MatrizA (entrada):
 def Requerida (dimensao, delta):
     Req = np.dot(Transposta(MatrizA(dimensao)),MatrizA(dimensao)) + delta*Identidade(dimensao*dimensao)
     return Req
+
+#Pega a imagem original e transforma em um vetor
+def getImageVector(pathIMG):
+    vectorimagens = plt.imread(pathIMG)
+    vectorok = vectorimagens.flatten('F')
+    return vectorok
+
+#Obtem a % do erro
+def getERRO(vectorimg, vector2):
+    i = 0
+    soma1 = 0
+    soma2 = 0
+    while i < len(vectorimg):
+        dif2 = (vectorimg[i] - vector2[i])**2
+        soma1 = soma1 + dif2
+        denominador = vectorimg[i]*vectorimg[i]
+        soma2 = soma2 + denominador
+        i = i + 1
+    sqrtsoma1 = math.sqrt(soma1)
+    sqrtsoma2 = math.sqrt(soma2)
+    return 100*(sqrtsoma1/sqrtsoma2)
+
+#Funções para fatoração e solução por LU
+def lenMatrix(M):
+	N = len(M[:, 0]) 
+	M = len(M[0, :])
+	return N, M
+
+def escalonamento(M):
+	vector = M.copy()
+	cross = MatrizZero(len(vector[0,:]), len(vector[:,0]))
+	for i in range(len(vector[:, 0])-1):
+		for k in range(i+1, len(vector[:,0])):
+			cross[k, i]  = vector[k, i]/vector[i,i]
+			temp = vector[i,] * cross[k, i]
+			fim = vector[k,] - temp
+			vector[k] = fim
+	for i in range(len(cross)):
+		cross[i, i] = 1
+	return vector, cross
+
+def MatL(L, vector):
+	linhas,colunas = lenMatrix(L)
+	R = vector
+	M = L
+	X  = MatrizZero(linhas, 1)
+	for i in range(linhas):
+		X[i] = R[i]
+		for j in range(i):
+			X[i] -= M[i][j]*X[j]
+		X[i] /= M[i][i]
+	return X
+
+def MatU(L, vector):
+	linhas,colunas = lenMatrix(L)
+	R = vector
+	M = L
+	X  = MatrizZero(linhas, 1)
+	for i in range(linhas-1,-1,-1):
+		X[i, 0] = R[i, 0]
+		for k in range(i+1, linhas):
+			X[i, 0] -= M[i][k]*X[k][0]
+		X[i, 0] /= M[i][i]
+	return X
+
+def SolutionWithLU(M, R):
+	resU, resL  = escalonamento(M)
+	Y = MatL(resL, R)
+	X = MatU(resU, Y)
+	return X
+#Fim da Fatoração e solução por LU
 
 #INICIO DO PROGRAMA
 
@@ -174,32 +163,58 @@ else:
 #Coletando vetor p
 p = load(pathnpy)
 n = int(len(p)/2) 
+
 #Calculando A e sua transposta 
 A = MatrizA(n)
 At = Transposta(A)
 
 #Req = At*A + delta*I (para deltas respectivos)
+Req0 = Requerida(n, 0)
 Req1 = Requerida(n, 0.001)
 Req2 = Requerida(n, 0.01)
 Req3 = Requerida(n, 0.1)
 
+#Calculando os Determinantes requeridos no Ex1
+det0 = Determinante(Req0)
+det1 = Determinante(Req1)
+det2 = Determinante(Req2)
+det3 = Determinante(Req3)
+
+#Escreve esses determinantes num txt de saida
+file = open("determinantesEx1.txt", "a+")
+str_dictionary0 = repr(det0)
+str_dictionary1 = repr(det1)
+str_dictionary2 = repr(det2)
+str_dictionary3 = repr(det3)
+file.write("Imagem: " + path + "\n" + "delta: 0 = " + str_dictionary0 + "\n" + "delta: 0.001 = " + str_dictionary1 + "\n" + "delta: 0.01 = " + str_dictionary2 + "\n" + "delta: 0.1 = " + str_dictionary3 + "\n")
+file.close()
+
 #Solução do sist linear
 newp = np.dot(At, p)
-f1 = achaSolucao(Req1, newp)
-f2 = achaSolucao(Req2, newp) 
-f3 = achaSolucao(Req3, newp)
+f1 = SolutionWithLU(Req1, newp)
+f2 = SolutionWithLU(Req2, newp) 
+f3 = SolutionWithLU(Req3, newp)
 
+#calculando erro
+vectorimg = getImageVector(pathimagem)
+errof1 = getERRO(vectorimg, f1)
+errof2 = getERRO(vectorimg, f2)
+errof3 = getERRO(vectorimg, f3)
+
+#Escreve esses erros num txt de saida
+file = open("errosEx1.txt", "a+")
+erro1 = repr(errof1)
+erro2 = repr(errof2)
+erro3 = repr(errof3)
+file.write("Imagem: " + path + "\n" + "delta 0.001, " + "erro: " + erro1 + "\n" + "delta 0.01, " + "erro: " + erro2 + "\n" + "delta 0.1, " + "erro: " + erro3 + "\n" + "\n")
+file.close()
+
+#ajeitando a matriz pra o formato ideal
 m1 = Transposta(f1.reshape(int(math.sqrt(len(f1))),int(math.sqrt(len(f1)))))
 m2 = Transposta(f2.reshape(int(math.sqrt(len(f2))),int(math.sqrt(len(f2)))))
 m3 = Transposta(f3.reshape(int(math.sqrt(len(f3))),int(math.sqrt(len(f3)))))
 
-
 #plotando imagens
 plotaImages(pathimagem, m1, m2, m3)
 
-
-# #Carrega a imagem original
-# img = mpimg.imread(pathimagem)
-# imgplot = plt.imshow(img)
-# plt.show()
-
+#Fim do programa
--- tomo2.py
+++ tomo2.py
@@ -1,54 +1,6 @@
 import math, matplotlib.pyplot as plt, numpy as np, pandas as pd, seaborn as sns, matplotlib.image as mpimg, pprint, sys, os
 from numpy import load
 
-#Fatoração e solução por LU
-def lenMatrix(matriz):
-	numero_linhas = len(matriz[:, 0]) 
-	numero_colunas = len(matriz[0, :])
-	return numero_linhas, numero_colunas
-
-def escalonamento(matriz):
-	array = matriz.copy()
-	multiplicadores = np.zeros([len(array[0,:]), len(array[:,0])])
-	for i in range(len(array[:, 0])-1):
-		for k in range(i+1, len(array[:,0])):
-			multiplicadores[k, i]  = array[k, i]/array[i,i]
-			linhaTemp = array[i,] * multiplicadores[k, i]
-			linhaFinal = array[k,] - linhaTemp
-			array[k] = linhaFinal
-	for i in range(len(multiplicadores)):
-		multiplicadores[i, i] = 1
-	return array, multiplicadores
-
-def resolveTriangularInf(matrizTriangular, respostas):
-	linhas,colunas = lenMatrix(matrizTriangular)
-	R = respostas
-	M = matrizTriangular
-	X  = np.zeros([linhas, 1])
-	for i in range(linhas):
-		X[i] = R[i]
-		for j in range(i):
-			X[i] -= M[i][j]*X[j]
-		X[i] /= M[i][i]
-	return X
-
-def resolveTriangularSup(matrizTriangular, respostas):
-	linhas,colunas = lenMatrix(matrizTriangular)
-	R = respostas
-	M = matrizTriangular
-	X  = np.zeros([linhas, 1])
-	for i in range(linhas-1,-1,-1):
-		X[i, 0] = R[i, 0]
-		for k in range(i+1, linhas):
-			X[i, 0] -= M[i][k]*X[k][0]
-		X[i, 0] /= M[i][i]
-	return X
-
-def achaSolucao(M, R):
-	upper, lower  = escalonamento(M)
-	Y = resolveTriangularInf(lower, R)
-	X = resolveTriangularSup(upper, Y)
-	return X
 # funções básicas
 
 # Identidade ordem n
@@ -66,11 +18,6 @@ def Determinante(A):
     B = np.linalg.det(A)
     return B
 
-# Inversa
-def Inversa (A):
-    B = np.linalg.inv(A)
-    return B
-
 #Devolve matriz mxn com zeros
 def MatrizZero (m, n):
     A = np.zeros((m,n))
@@ -83,28 +30,18 @@ def DiagSec (ordem):
 
 #funcoes particulares
 
-#Matriz n-ésima linha de UNS
-def Xesima (x, lin, col): 
-    XesimaM = [[0 for x in range(lin)] for y in range(col)] 
-    for i in range(lin):
-        for j in range(col):
-            if i == x-1:
-                XesimaM[i][j] = 1
-            else:
-                XesimaM[i][j] = 0
-    return XesimaM
-
 #Devolve matriz mxn com zeros e linha x com UNS
 def MatrizNUns (m, n, x):
-    B = np.zeros((m,n))
+    Matrix = np.zeros((m,n))
     for i in range(m):
         for j in range(n):
             if i == x-1:
-                B[i][j] = 1
+                Matrix[i][j] = 1
             else:
-                B[i][j] = 0
-    return B
+                Matrix[i][j] = 0
+    return Matrix
 
+#função auxiliar para montar A
 def deslocaMatriz(A):
     m = A.shape[0]
     n = A.shape[1]
@@ -116,6 +53,8 @@ def deslocaMatriz(A):
             else:
                 B[i][j] = A[i-1][j-1]
     return B
+
+#função auxiliar para montar A
 def deslocaMatriz2(A, dimensional):
     m = A.shape[0]
     n = A.shape[1]
@@ -134,6 +73,7 @@ def first(dimensao):
     A[0][0]=1
     return A
 
+#função auxiliar para montar A
 def MatrizBelow(dimensao):
     A = np.kron(np.identity(dimensao), first(dimensao))
     B = np.zeros((dimensao -1,dimensao*dimensao)) 
@@ -147,6 +87,7 @@ def MatrizBelow(dimensao):
         i = i + 1
     return R +C
 
+#função auxiliar para montar A
 def MatrizUp(dimensao):
     A = DiagSec(dimensao)
     B = np.zeros((dimensao -1,dimensao)) 
@@ -162,6 +103,7 @@ def MatrizUp(dimensao):
         i = i + 1
     return R + E
 
+#plota a solução
 def plotaImages(image, sol1, sol2, sol3):
     plt.figure(figsize=(10,6))
     plt.subplot(2,2,1)
@@ -188,13 +130,14 @@ def plotaImages(image, sol1, sol2, sol3):
 
     plt.show()
 
-#Matriz A
+#Matriz A do ex1
 def MatrizA (entrada):
     ParteSuperior = np.kron(MatrizNUns(1, entrada, 1), Identidade(entrada))
     ParteInferior = np.kron(Identidade(entrada), MatrizNUns(1, entrada, 1))
     Soma = np.kron(MatrizNUns(2,1,1), ParteSuperior) + np.kron(MatrizNUns(2,1,2), ParteInferior)
     return Soma
 
+#Matriz A do ex2
 def MatrizA2(dimensao):
     A = MatrizA(dimensao)
     B = MatrizUp(dimensao)
@@ -207,6 +150,78 @@ def MatrizA2(dimensao):
 def Requerida (dimensao, delta):
     Req = np.dot(Transposta(MatrizA(dimensao)),MatrizA(dimensao)) + delta*Identidade(dimensao*dimensao)
     return Req
+
+#Matriz Requerida At*A + delta*I
+def RequeridaA2 (dimensao, delta):
+    Req = np.dot(Transposta(MatrizA2(dimensao)),MatrizA2(dimensao)) + delta*Identidade(dimensao*dimensao)
+    return Req
+
+#pega o vetor correspondente à imagem
+def getImageVector(pathIMG):
+    vectorimagens = plt.imread(pathimagem)
+    vectorok = vectorimagens.flatten('F')
+    return vectorok
+
+#pega o erro
+def getERRO(vectorimg, vector2):
+    i = 0
+    soma1 = 0
+    soma2 = 0
+    while i < len(vectorimg):
+        dif2 = (vectorimg[i] - vector2[i])**2
+        soma1 = soma1 + dif2
+        denominador = vectorimg[i]*vectorimg[i]
+        soma2 = soma2 + denominador
+        i = i + 1
+    sqrtsoma1 = math.sqrt(soma1)
+    sqrtsoma2 = math.sqrt(soma2)
+    return 100*(sqrtsoma1/sqrtsoma2)
+
+#Funções para fatoração e solução por LU
+def lenMatrix(M):
+	N = len(M[:, 0]) 
+	M = len(M[0, :])
+	return N, M
+def escalonamento(M):
+	vector = M.copy()
+	cross = np.zeros([len(vector[0,:]), len(vector[:,0])])
+	for i in range(len(vector[:, 0])-1):
+		for k in range(i+1, len(vector[:,0])):
+			cross[k, i]  = vector[k, i]/vector[i,i]
+			temp = vector[i,] * cross[k, i]
+			fim = vector[k,] - temp
+			vector[k] = fim
+	for i in range(len(cross)):
+		cross[i, i] = 1
+	return vector, cross
+def MatL(L, vector):
+	linhas,colunas = lenMatrix(L)
+	R = vector
+	M = L
+	X  = np.zeros([linhas, 1])
+	for i in range(linhas):
+		X[i] = R[i]
+		for j in range(i):
+			X[i] -= M[i][j]*X[j]
+		X[i] /= M[i][i]
+	return X
+def MatU(L, vector):
+	linhas,colunas = lenMatrix(L)
+	R = vector
+	M = L
+	X  = np.zeros([linhas, 1])
+	for i in range(linhas-1,-1,-1):
+		X[i, 0] = R[i, 0]
+		for k in range(i+1, linhas):
+			X[i, 0] -= M[i][k]*X[k][0]
+		X[i, 0] /= M[i][i]
+	return X
+def SolutionWithLU(M, R):
+	resU, resL  = escalonamento(M)
+	Y = MatL(resL, R)
+	X = MatU(resU, Y)
+	return X
+#Fim da Fatoração e solução por LU
 
 #INICIO DO PROGRAMA
 
@@ -233,23 +248,51 @@ A = MatrizA2(n)
 At = Transposta(A)
 
 #Req = At*A + delta*I (para deltas respectivos)
-Req1 = Requerida(n, 0.001)
-Req2 = Requerida(n, 0.01)
-Req3 = Requerida(n, 0.1)
+Req0 = RequeridaA2(n, 0)
+Req1 = RequeridaA2(n, 0.001)
+Req2 = RequeridaA2(n, 0.01)
+Req3 = RequeridaA2(n, 0.1)
+
+#Calculando os Determinantes requeridos no Ex1
+det0 = Determinante(Req0)
+det1 = Determinante(Req1)
+det2 = Determinante(Req2)
+det3 = Determinante(Req3)
+
+#Escreve esses determinantes num txt de saida
+file = open("determinantesEx2.txt", "a+")
+str_dictionary0 = repr(det0)
+str_dictionary1 = repr(det1)
+str_dictionary2 = repr(det2)
+str_dictionary3 = repr(det3)
+file.write("Imagem: " + path + "\n" + "n = " + str(n) + "\n" + "delta: 0 = " + str_dictionary0 + "\n" + "delta: 0.001 = " + str_dictionary1 + "\n" + "delta: 0.01 = " + str_dictionary2 + "\n" + "delta: 0.1 = " + str_dictionary3 + "\n")
+file.close()
 
 #Solução do sist linear
 newp = np.dot(At, p)
-print(len(newp))
-f1 = achaSolucao(Req1, newp)
-f2 = achaSolucao(Req2, newp)
-f3 = achaSolucao(Req3, newp)
+f1 = SolutionWithLU(Req1, newp)
+f2 = SolutionWithLU(Req2, newp)
+f3 = SolutionWithLU(Req3, newp)
 
+#calculando erro
+vectorimg = getImageVector(pathimagem)
+errof1 = getERRO(vectorimg, f1)
+errof2 = getERRO(vectorimg, f2)
+errof3 = getERRO(vectorimg, f3)
+
+#Escreve esses erros num txt de saida
+file = open("errosEx2.txt", "a+")
+erro1 = repr(errof1)
+erro2 = repr(errof2)
+erro3 = repr(errof3)
+file.write("Imagem: " + path + "\n" + "delta 0.001, " + "erro: " + erro1 + "\n" + "delta 0.01, " + "erro: " + erro2 + "\n" + "delta 0.1, " + "erro: " + erro3 + "\n" + "\n")
+file.close()
+
+#ajeitando a matriz pra o formato ideal
 tamanho = int(math.sqrt(len(newp)))
-print (f1)
 m1 = Transposta(f1.reshape(tamanho, tamanho))
 m2 = Transposta(f2.reshape(tamanho, tamanho))
 m3 = Transposta(f3.reshape(tamanho, tamanho))
 
 #plotando imagens
 plotaImages(pathimagem, m1, m2, m3)
-print(m1)
